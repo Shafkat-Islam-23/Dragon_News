ddccd338c80ae982264e09b9f8d2c17fbe06da87
Shafkat Islam <shafkatislam.12@gmail.com>
2026-05-16 01:16:24 +0600

    final

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,10 +1,37 @@
-import React from "react";
-import { useParams } from "react-router";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
+  const { id } = useParams();
+  const data = useLoaderData();
 
-  const {id} = useParams();
-  return <div>main section {id}</div>;
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+      console.log(categoryNews.length);
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true,
+      );
+      setCategoryNews(filteredNews);
+      console.log(categoryNews.length);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      setCategoryNews(filteredNews);
+      console.log(categoryNews.length);
+    }
+  }, [data, id]);
+
+  console.log(categoryNews);
+
+  return (
+    <div>
+      main section {id} aa {categoryNews.length}
+    </div>
+  );
 };
 
 export default CategoryNews;
